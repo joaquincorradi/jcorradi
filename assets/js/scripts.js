10f347c9817423b7f1e03ebb1c328a97bf754ebf
Joaquin I. Corradi (macOS) <corradijoaquin@gmail.com>
2023-08-14 11:13:15 -0300

    Startig responsive design

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelector(".hamburger-menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList("open");
+  icon.classList("ri-menu-unfold-line");
+}
