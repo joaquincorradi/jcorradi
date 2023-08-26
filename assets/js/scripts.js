374f454b314462c7ccd7180f07d26a6cc74de7ff
Joaquin I. Corradi (Fedora Linux) <corradijoaquin@gmail.com>
2023-08-25 22:51:08 -0300

    Light mode fixes and uc. page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,4 +10,13 @@ function toggleMenu() {
 function lightMode() {
   var body = document.body;
   body.classList.toggle("light-mode");
+
+  var icon = document.getElementById("theme-icon-desk");
+  if (body.classList.contains("light-mode")) {
+    icon.classList.remove("ri-sun-line");
+    icon.classList.add("ri-moon-fill");
+  } else {
+    icon.classList.remove("ri-moon-fill");
+    icon.classList.add("ri-sun-line");
+  }
 }
