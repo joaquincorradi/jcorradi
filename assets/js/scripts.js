d08eb8583a4a8622881a1e82005411306864d0cd
Joaquin I. Corradi (Fedora Linux) <corradijoaquin@gmail.com>
2023-09-17 04:12:00 -0300

    Adding nav-rail

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,36 +1,38 @@
-function toggleMenu() {
-  const menu = document.querySelector(".hamburger-menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  const closeIcon = document.querySelector("ri-close-line");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-  closeIcon.classList.toggle("open");
-}
-
 function lightMode() {
   var body = document.body;
   body.classList.toggle("light-mode");
 
-  var icon = document.getElementById("theme-icon-desk");
-  if (body.classList.contains("light-mode")) {
-    icon.classList.remove("ri-sun-line");
-    icon.classList.add("ri-moon-fill");
-    localStorage.setItem("theme", "light");
-  } else {
-    icon.classList.remove("ri-moon-fill");
-    icon.classList.add("ri-sun-line");
-    localStorage.setItem("theme", "dark");
-  }
+  // var icon = document.getElementById("theme-icon-desk");
+  // if (body.classList.contains("light-mode")) {
+  //   icon.classList.remove("ri-sun-line");
+  //   icon.classList.add("ri-moon-fill");
+  //   localStorage.setItem("theme", "light");
+  // } else {
+  //   icon.classList.remove("ri-moon-fill");
+  //   icon.classList.add("ri-sun-line");
+  //   localStorage.setItem("theme", "dark");
+  // }
 }
 
-function loadTheme() {
-  var theme = localStorage.getItem("theme");
-  if (theme === "light") {
-    document.body.classList.add("light-mode");
-    document.getElementById("theme-icon-desk").classList.add("ri-moon-fill");
-  }
+// Función para actualizar el color de fondo del enlace activo en la barra lateral
+function updateActiveLink() {
+  const sections = document.querySelectorAll(".section");
+  const navIcons = document.querySelectorAll(".nav-rail__icon");
+
+  sections.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+      // La sección está visible en la ventana
+      navIcons[index].classList.add("active-icon");
+    } else {
+      // La sección no está visible en la ventana
+      navIcons[index].classList.remove("active-icon");
+    }
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  loadTheme();
-});
+// Escuchar el evento de desplazamiento (scroll) para actualizar el enlace activo
+window.addEventListener("scroll", updateActiveLink);
+
+// Llamar a la función al cargar la página para establecer el estado inicial
+updateActiveLink();
